518682bbcf5b672d4ff63df9be5c878f42912318
Zac <60723688+TubeGolem@users.noreply.github.com>
2023-08-19 16:09:08 -0600

    Change Dynamic Platforms to MonoBehavior

--- Assets/DynamicPlatforms/DynamicPlatform.cs
+++ Assets/DynamicPlatforms/DynamicPlatform.cs
@@ -2,14 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "WrenchDog/DynamicPlatform")]
-public class DynamicPlatform : ScriptableObject
+public class DynamicPlatform : MonoBehaviour
 {
+    
     public bool isLocked = false;
-    public Sprite sprite;
-    public SpriteRenderer spriteRenderer;
-    public BoxCollider2D boxCollider2D;
-    public Rigidbody2D rigidbody2D;
 
     // Start is called before the first frame update
     void Start()
