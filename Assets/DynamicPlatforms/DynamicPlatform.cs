a467dc31958ec6b2453446af796aedf3e0c0d441
Zac <60723688+TubeGolem@users.noreply.github.com>
2023-08-19 18:12:21 -0600

    Add left and right drop platforms

--- Assets/DynamicPlatforms/DynamicPlatform.cs
+++ Assets/DynamicPlatforms/DynamicPlatform.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DynamicPlatform : MonoBehaviour
 {
-    
+
     public bool isLocked = false;
+    public GameObject child;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,4 +20,11 @@ public class DynamicPlatform : MonoBehaviour
     {
         
     }
+
+    void ToggleFreezePlatform()
+    {
+        Rigidbody2D rb = child.GetComponent<Rigidbody2D>();
+        rb.freezeRotation = !rb.freezeRotation;
+    }
+
 }
