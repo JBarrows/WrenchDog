69af193e73054d1ad48dd5976d78e4dd6089db63
Joel Barr <Joelbarr1220@gmail.com>
2023-08-19 18:44:09 -0600

    Removed debug message, removed pit trap

--- Assets/Scripts/AligatorController.cs
+++ Assets/Scripts/AligatorController.cs
@@ -40,11 +40,7 @@ public class AligatorController : MonoBehaviour
                 return false;
             
             RaycastHit2D hit = Physics2D.Linecast(transform.position + new Vector3(-0.73f / 2F, -0.01f), transform.position + new Vector3(0.73f/2F, -0.01f));
-            if (hit && hit.collider != null) {
-                Debug.Log("hit");
-            } else {
-                // Debug.Log("NOT");
-            }
+            
             if (!hit) {
                 return false;
             }
