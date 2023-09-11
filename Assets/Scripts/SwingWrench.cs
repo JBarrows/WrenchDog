de8a41211e15e9c55de1e3c5d39f30a650e825b0
Joel Barr <Joelbarr1220@gmail.com>
2023-09-11 12:42:19 -0600

    SwingWrench and SwingPoint rotating correctly

--- Assets/Scripts/SwingWrench.cs
+++ Assets/Scripts/SwingWrench.cs
@@ -22,13 +22,18 @@ public class SwingWrench : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (handleObject && holderPoint) {
-            handleObject.transform.position = holderPoint.position;
-        }
+        if (!holderPoint || !swingPoint)
+            return;
 
-        if (headObject && swingPoint) {
-            headObject.transform.position = swingPoint.transform.position;
-        }
+        handleObject.transform.position = holderPoint.position;
+        headObject.transform.position = swingPoint.transform.position;
+
+        // Rotate the Handle to face the Head
+        Vector3 wrenchVector = handleObject.transform.position - headObject.transform.position;
+        Quaternion r = Quaternion.Euler(0.0f, 0.0f, Mathf.Atan2(wrenchVector.y, wrenchVector.x) * Mathf.Rad2Deg);
+        handleObject.transform.rotation = r;
+        headObject.transform.rotation = r;
+        swingPoint.transform.rotation = r;
 
         // Align the shaft
         shaftLine.useWorldSpace = true;
