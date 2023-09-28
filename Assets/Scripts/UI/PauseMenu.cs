c68116033bb3cc095d72193246388c1fae47f4b3
Joel Barr <joelbarr1220@gmail.com>
2023-09-28 12:34:08 -0600

    Added an audio mixer, and changed volume persists between scenes

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public Animator animator;
+    public AudioMixer masterMixer;
+    public Slider musicSlider;
+
+    static float musicValue = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (musicSlider != null)
+            musicSlider.value = musicValue;
     }
 
     // Update is called once per frame
@@ -33,10 +40,13 @@ public class PauseMenu : MonoBehaviour
         Application.Quit();
     }
 
-    public void setVolume(float value)
+    public void setMusicVolume(float value)
     {
-        var audio = FindAnyObjectByType<AudioSource>();
-        if (audio)
-            audio.volume = value;
+        if (!masterMixer)
+            return;
+        
+        musicValue = value;
+        var dbValue = Mathf.Log(Mathf.Max(0.001f, musicValue)) * 20;
+        masterMixer.SetFloat("masterVol", dbValue);
     }
 }
